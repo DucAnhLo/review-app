11a48d1eec9ccceb5c9c718d25c2d2ee384dcd01
ducanhlo <liamloducanh@gmail.com>
2023-04-01 18:15:18 +0100

    Added seperate css class for Bookings

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,49 +1,50 @@
-import React from 'react'
+import React from 'react';
 import { useTheme } from '@material-ui/core/styles';
 import { Box, Typography, Button, Card, CardContent, CardActions } from '@material-ui/core';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import constants from '../constants';
+import './Bookings.css';
 
 function Booking() {
 
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const notify = () => toast('Success!');
-    const { primaryColor, subPrimaryColor, salePrice, normalPrice } = constants;
+  const notify = () => toast('Success!');
+  const { primaryColor, subPrimaryColor, salePrice, normalPrice } = constants;
 
   return (
-    <Box style={{height: '200px', width:'600px' }}>
-        <Card style={{width:'500px', height:'400px', position: 'relative'}}>
-        <CardContent style={{marginBottom:'40px'}}>
-            <Typography variant="h4" component="div">
-                4 days - 3 nights
+    <Box style={{ height: '200px', width: '600px' }}>
+      <Card className="booking-card">
+        <CardContent className="booking-card-content">
+          <Typography variant="h4" component="div">
+            4 days - 3 nights
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Hanoi to Ho Chi Minh
+          </Typography>
+          <Box style={{ marginTop: '20px' }}>
+            <Typography styles={{ marginTop: '1rem', color: 'white' }} variant="body1">
+              From
             </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Hanoi to Ho Chi Minh
-            </Typography>
-            <Box style={{marginTop:'20px'}}>
-                <Typography styles={{marginTop:'1rem', color:'white'}} variant="body1">
-                    From
-                </Typography>
-                <Box style={{display:'flex',justifyContent:'space-between'}}>
-                    <Typography variant="h4">
-                        {salePrice.toLocaleString()} vnd
-                    </Typography>
-                    <Typography variant="h6" style={{color:'grey'}}>
-                        Was {normalPrice.toLocaleString()} vnd
-                    </Typography>
-                </Box>
+            <Box style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Typography variant="h4">
+                {salePrice.toLocaleString()} vnd
+              </Typography>
+              <Typography variant="h6" style={{ color: 'grey' }}>
+                Was {normalPrice.toLocaleString()} vnd
+              </Typography>
             </Box>
-            <Box style={{ position: 'absolute', top: 0, right: 0, backgroundColor: primaryColor, color: subPrimaryColor, borderRadius: '0 0 0 25px', padding: '0.5rem 1rem', fontWeight: 'bold' }}>
-                On Sale
-            </Box>
+          </Box>
+          <Box className="booking-card-sale">
+            On Sale
+          </Box>
         </CardContent>
         <CardActions>
-            <Button onClick={notify} style={{color: subPrimaryColor, backgroundColor: primaryColor, borderRadius:'25px', width:'100%'}} variant='contained' size="large">Book Now</Button>
+          <Button onClick={notify} className="booking-card-button" variant='contained' size="large">Book Now</Button>
         </CardActions>
-        </Card>
-        <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+      </Card>
+      <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
     </Box>
   )
 }
